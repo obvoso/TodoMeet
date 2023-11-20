b8509b5719b75c9c4baeff452c5360f5679640af
obvoso <autumninmoon@gmail.com>
2023-11-20 13:35:48 +0900

    feat: login page

---
+++ app/src/main/java/com/example/todomeet/login/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.todomeet.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todomeet.MainActivity;
+import com.example.todomeet.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_button);
+
+        ImageButton mainLoginButton = findViewById(R.id.mainLoginButton);
+        mainLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
